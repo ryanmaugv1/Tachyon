ea60934503b48c0db16cd45a014cad86b2418151
Ryan Maugin <ryanmauginn@gmail.com>
2017-05-27 21:21:36 +0100

    Created openSourceFile function to read tachus source code file content

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -7,8 +7,39 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+// Function Declration
+string openSourceFile();
+
 
 int main () {
-    std::cout << "Hello World!" << std::endl;
+
+    string data = openSourceFile();
+
     return 0;
+}
+
+
+/**
+ * Open Source File
+ * This will open the source file and save it's content
+ * @return fileContent
+ */
+string openSourceFile () {
+    ifstream sourceFile("./src/test.txt"); // Open file
+    string sourceFileLine;
+    string fileContent;
+
+    // Loop through each line and store it in fileContent
+    while ( sourceFile.good() ) {
+        getline(sourceFile, sourceFileLine);
+        // if sourceFileLine is empty then don't save it
+        if (sourceFileLine != "") fileContent += sourceFileLine + "\n";
+    }
+
+    sourceFile.close(); // Close file
+    return fileContent;
 }
