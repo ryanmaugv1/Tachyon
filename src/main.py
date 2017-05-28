2a61d4746533d879acf87fd38e44562be48f5ca5
Ryan Maugin <ryanmauginn@gmail.com>
2017-05-28 18:22:46 +0100

    Linked main file to lexer

---
+++ src/main.py
@@ -0,0 +1,25 @@
+#
+#  Tachus
+#  main.py
+#
+#  Created on 27/05/17
+#  Ryan Maugin <ryan.maugin@adacollege.org.uk>
+#
+
+import lexer
+import os
+
+def lexical_analyzer():
+    # Create an instance of the lexer class
+    lex = lexer.Lexer()
+    content = ""
+
+    # Open source code file and get it's content
+    with open(os.path.dirname(os.path.realpath(__file__)) + "/test.txt", "r") as file:
+        content = file.read()
+    
+    # Call lexer method to perform lexical analysis on code
+    tokens = lex.tokenize(content)
+
+lexical_analyzer()
+
