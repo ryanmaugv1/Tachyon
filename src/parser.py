6ae750901bc85c15961e30c0765f033bd969d4e3
Ryan Maugin <ryanmauginn@gmail.com>
2017-06-06 12:57:47 +0100

    Added parser baisc method skeletons

--- src/parser.py
+++ src/parser.py
@@ -18,3 +18,48 @@ class Parser(object):
          token_stream (list) : The tokens produced by lexer
         """
         print(token_stream)
+
+
+    def parse_if_statement(self, token_stream):
+        """ Parsing If Statement
+        
+        This will parse through an if statement and create it's abstract tree and handle any
+        syntax error
+
+        Args:
+            token_stream (list) : List of tokens starting from where if statement was found
+
+        Returns:
+            AST : The if statement abstract syntax tree
+        """
+        print("IF STATEMENT")
+
+
+    def parse_variable_decleration(self, token_stream):
+        """ Parsing Variable decleration
+
+        This will parse through a variable decleration and create it's abstract tree and handle any
+        syntax errors
+
+        Args:
+            token_stream (list) : List of tokens starting from where if statement was found
+
+        Returns:
+            AST : The variable decleration abstract syntax tree
+        """
+        print("VARIABLE DECLERATION")
+
+    
+    def parse_print(self, token_stream):
+        """ Parsing Variable decleration
+
+        This will parse through a variable decleration and create it's abstract tree and handle any
+        syntax errors
+
+        Args:
+            token_stream (list) : List of tokens starting from where if statement was found
+
+        Returns:
+            AST : The variable decleration abstract syntax tree
+        """
+        print("PRINT")
