d43dd080b38e66e05ec045bcc8781cc56abce222
Ryan Maugin <ryanmauginn@gmail.com>
2017-06-11 01:20:05 +0100

    Added test case for parser

---
+++ tests/parser_tests.py
@@ -0,0 +1,31 @@
+#
+#  Tachyon
+#  parser_tester.py
+#
+#  Created on 11/06/17
+#  Ryan Maugin <ryan.maugin@adacollege.org.uk>
+#
+
+import unittest
+import sys, os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir)))
+import src.parser
+
+
+class ParserTestCase(unittest.TestCase):
+    """ Tests for parser methods """
+
+    # Create parser instance to test on
+    parserObject = src.parser.Parser([['DATATYPE', 'int'], ['IDENTIFIER', 'a'], ['OPERATOR', '='], ['INTEGER', '11'], ['STATEMENT_END', ';'], 
+                                      ['DATATYPE', 'str'], ['IDENTIFIER', 'name'], ['OPERATOR', '='], ['STRING', '"Ryan Maugin The Boss"'], ['STATEMENT_END', ';']])
+
+    
+    # 
+    #
+    #  TESTS for parser_variable_decleration() method
+    #
+    #
+    
+
+if __name__ == '__main__':
+    unittest.main()
