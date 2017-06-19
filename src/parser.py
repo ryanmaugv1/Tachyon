ed5a391faa6b8bd1e151b9999bdeee925fc97cb5
Ryan Maugin <ryanmauginn@gmail.com>
2017-06-19 09:59:41 +0100

    Added variable decleration parsing

--- src/parser.py
+++ src/parser.py
@@ -34,10 +34,6 @@ class Parser(object):
         Args:
          token_stream (list) : The tokens produced by lexer
         """
-        print('---------------------------------------------')
-        print(token_stream)
-        print('---------------------------------------------')
-        print("//////////////// DEBUG ZONE ////////////////")
 
         # Loop through each token
         while self.token_index < len(token_stream):
@@ -48,9 +44,85 @@ class Parser(object):
 
             # This will check for an if statement token
             if token_type == 'IDENTIFIER' and token_value.lower() == 'if':
-                print('-----')
-
+                self.parse_if_statement(self.token_index, token_stream[self.token_index:len(token_stream)])
 
             # This will parse for a vraible decleration token
             if token_type == 'DATATYPE' and token_value.lower() in constants.DATATYPE:
-                print(token_value)
+                self.token_index += self.parse_variable_decleration(self.token_index, token_stream[self.token_index:len(token_stream)])
+                continue
+
+            # Increment index of source code token being checked
+            self.token_index += 1
+        
+        print(self.source_ast)
+        print('############################')
+        print(self.token_stream)
+
+
+
+    def error_message(self, token_index, error_message):
+        print(error_message)
+        quit()
+
+
+    
+    def does_var_exist(self, var_name):
+        # This loops through the symbol tree and check every variable name to see if there are any that match
+        for item in self.symbol_tree:
+            if item[0] == var_name: 
+                self.error_message(self.token_index, 'SemanticError: Variable "' + var_name + '" already exists!')
+        return False
+
+
+
+    def remove_checked_tokens(self, start_index, tokens_checked):
+        for x in range(start_index, start_index + tokens_checked):
+            print(self.token_stream[x])
+            self.token_stream[x] = '-'
+        print('--------')
+        
+        
+    def parse_variable_decleration(self, token_index, token_stream):
+        
+        # The Variable Decleration AST
+        ast = { 'VariableDecleration': [] }
+        # Marks if the terminator ';' is found
+        found_terminator = False
+        # Hold the number of tokens checked
+        tokens_checked = 0
+
+        # Loop through the source code and form the parse tree for the var decleration
+        for x in range(0, len(token_stream)):
+            tokens_checked += 1
+
+            # Check if the end statement is found and exit loop and form parse tree
+            if token_stream[x][1] == ';': 
+                found_terminator = True   
+                break
+
+            # This will check the variable data type and add it to the ast
+            if token_stream[x][0] == 'DATATYPE': 
+                ast['VariableDecleration'].append({ 'type': token_stream[x][1] })
+
+            # Check variable name and add it to symbol tree if it doesn't already exist
+            if token_stream[x][0] == 'IDENTIFIER' and x == 1: 
+                if self.does_var_exist(token_stream[x][1]) == False:
+                    ast['VariableDecleration'].append({ 'name': token_stream[x][1] })
+            
+            # Get the value of the variable decleration
+            if token_stream[x][0] == 'INTEGER' or token_stream[x][0] == 'STRING' and x == 3:
+                print('ran')
+                ast['VariableDecleration'].append({ 'value': token_stream[x][1] })
+
+        # If the end statement was never found then throw an error
+        if found_terminator == False: self.error_message(token_index, 'SyntaxError: Variable Decleration did not end with ";"')
+
+        # Add variable decleration ast to full source code ast
+        self.source_ast['main_scope'].append(ast)
+        self.remove_checked_tokens(token_index, tokens_checked)
+        return tokens_checked
+
+        
+
+    def parse_if_statement(self, token_index, token_stream):
+        print('IF STATEMENT')
