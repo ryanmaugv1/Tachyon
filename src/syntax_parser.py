b4367948d04da2e1c2c2b0453a5cda1daefa3736
Ryan Maugin <ryanmauginn@gmail.com>
2017-07-15 19:02:15 +0100

    Added ability to parse concatenation and fetch value of defined variable by name

--- src/syntax_parser.py
+++ src/syntax_parser.py
@@ -16,7 +16,7 @@ class SyntaxParser(object):
         # Complete Abstract Syntax tree
         self.source_ast = { 'main_scope': [] }
         # Symbol table fo variable semantical analysis
-        self.symbol_tree = []
+        self.symbol_tree = [ ['last_name', 'Maugin'], ['test', 'test101'] ]
         # This will hold all the tokens
         self.token_stream = token_stream
         # This will hold the token index we are parsing at
@@ -104,8 +104,10 @@ class SyntaxParser(object):
                     tokens_checked += 1 # Indent the tokens checked within this for loop
 
                 # Call the equation parser and append value of successful or try tring concat parser if an error occurs
-                try:    ast['VariableDecleration'].append({ "value": self.equation_parser(value_list) })
-                except: print("Use string concat var value parsing")
+                try: ast['VariableDecleration'].append({ "value": self.equation_parser(value_list) })
+                except:
+                    try:    ast['VariableDecleration'].append({ "value": self.concatenation_parser(value_list) })
+                    except: print("ERROR: Variable decleration is invalid!")
                 break                   # Break out of the current var parsing loop since we just parsed everything
 
             tokens_checked += 1         # Indent within overall for loop
@@ -149,4 +151,70 @@ class SyntaxParser(object):
             elif item % 2 == 0: pass
         
         return total
-        
+
+
+
+    def concatenation_parser(self, concatenation_list):
+        """ Concatenaion Parser
+
+        This will parse concatenation of strings and variables with string values or integer
+        values to concatenate arithmetics to strings e.g. "Ryan is ", 10 + 6, "!"
+
+        args:
+            concatenation_list (list) : Array with all items needed seperated to perform concatenation
+        return:
+            value (string)            : Full string after concatenation done
+            error (list)              : Return False with an error message in a list
+        """
+
+        full_string = ""
+
+        for item in range(0, len(concatenation_list)):
+            
+            current_value = concatenation_list[item]
+
+            # Add the first item to the string
+            if item == 0:
+                
+                # This checks if the value being checked is a string or a variable
+                # If it is a string then just add it without the surrounding quotes
+                if current_value[0] == '"': full_string += current_value[1:len(current_value) - 1]
+                # If it isn't a string then get the variable value and append it
+                else: full_string += self.get_variable_value(current_value)
+                pass
+            
+            # This will check for the concatenation operator
+            if item % 2 == 1:
+                if current_value == "+": 
+                    # This checks if the value being checked is a string or a variable
+                    if concatenation_list[item + 1][0] != '"': full_string += self.get_variable_value(concatenation_list[item + 1])
+                    else: full_string += concatenation_list[item + 1][1:len(concatenation_list[item + 1]) - 1]
+                        
+                elif current_value == ",": 
+                    full_string += " " + concatenation_list[item + 1]
+
+                else: print("Error finding concatenation operand!")
+            
+            # This will skip value as it is already being added and dealt with when getting the operand
+            if item % 2 == 0: pass
+
+        return full_string
+
+
+
+    def get_variable_value(self, name):
+        """ Get Variable Value
+
+        This will get the value of a variable from the symbol tree and return the value
+        if the variable exists or an error if it doesn't
+
+        args:
+            name (string)  : The name which we will search for in symbol tree
+        returns:
+            value (string) : The value of the variable if it is found
+            error (bool)   : Sends back False if it was not found
+        """
+
+        for var in self.symbol_tree:
+            if var[0] == name: return var[1]
+        return False
