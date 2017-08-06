160d1a9388b2b3fdae1457c1b83d2bce64c1b081
Ryan Maugin <ryanmauginn@gmail.com>
2017-08-06 16:22:25 +0100

    Added objgen.py to generate AST objects

---
+++ src/objgen.py
@@ -0,0 +1,12 @@
+#
+#  Tachyon
+#  objgen.py
+#
+#  Created on 06/08/17
+#  Ryan Maugin <ryan.maugin@adacollege.org.uk>
+#
+
+class ObjectGenerator():
+
+    def __init__(self, source_ast):
+        print(source_ast)
