493aa9edfb70e12898f4ab99e4a7820fed2e72a3
Ryan Maugin <ryanmauginn@gmail.com>
2017-08-14 18:07:58 +0100

    Added setup.py for tachyon setup procedure

---
+++ setup.py
@@ -0,0 +1,12 @@
+#
+#  Tachyon
+#  setup.py
+#
+#  Created on 14/08/17
+#  Ryan Maugin <ryan.maugin@adacollege.org.uk>
+#
+
+class Setup():
+
+    def __init__(self):
+        print('Installing Tachyon!')
